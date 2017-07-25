5e8c723f77979b01bc6980578bd82c7cbe1e7814
huangzongjian <1593860024@qq.com>
2017-07-25 23:15:54 +0800

    idae整合git

---
+++ src/main/java/com/xulifei/Demo.java
@@ -0,0 +1,12 @@
+package com.xulifei;
+
+
+/**
+ * Created by john on 2017/7/25.
+ */
+public class Demo {
+
+    public void a(){
+        System.out.println("xulifei");
+    }
+}
---
+++ src/test/java/com/xulifei/testDemo.java
@@ -0,0 +1,14 @@
+package com.xulifei;
+
+import org.junit.Test;
+
+/**
+ * Created by john on 2017/7/25.
+ */
+public class testDemo {
+
+     @Test
+    public void a(){
+         System.out.println("xulifei");
+     }
+}
